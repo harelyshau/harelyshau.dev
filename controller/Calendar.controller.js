97497dc21a91bd657bd800f23556a5a9509c9d1d
Pavel Harelyshau <pavel@harelyshau.dev>
2023-06-08 10:21:36 +0400

    Init Google Calendar API

--- controller/Calendar.controller.js
+++ controller/Calendar.controller.js
@@ -1,21 +1,68 @@
 sap.ui.define([
     "./BaseController",
-], function (BaseController) {
+], (BaseController) => {
+
     "use strict"
+
     return BaseController.extend("pharelyshau.controller.Detail", {
 
-        onInit: function () {
-            let oRouter = this.getOwnerComponent().getRouter()
-            this.getRouter().getRoute("calendar").attachPatternMatched(this._onObjectMatched, this)
+        onInit() {
+            this.loadGoogleAPI();
         },
 
-        _onObjectMatched: function (oEvent) {
-            let oPath = window.decodeURIComponent(oEvent.getParameter("arguments").videoItemPath)
 
-            this.getView().bindElement({
-                path: "/" + oPath,
-                model: "youTubeModel"
-            })
+        onPressTest() {
+            this.loadGoogleAPI();
+
+
+            // $.ajax({
+            //     url: "https://reqres.in/api/products/3",
+            //     success: function (sResult) {
+            //         console.log(sResult);
+            //     }
+            // });
+        },
+
+        loadGoogleAPI() {
+            const script = document.createElement('script');
+            script.src = 'https://apis.google.com/js/api.js';
+
+            script.onload = () => {
+                gapi.load('client', () => {
+                    this.initGoogleAPI();
+                });
+            };
+
+            document.head.appendChild(script);
+        },
+
+        initGoogleAPI() {
+            const sApiKey = "AIzaSyD2O1sxa8AcJxvF0XQko-TwBD4TwdOv0SM";
+            gapi.client.init({
+                apiKey: sApiKey,
+                discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+                scope: 'https://www.googleapis.com/auth/calendar.readonly'
+            }).then(() => {
+                this.getAppointments();
+            }, (error) => {
+                console.error('Error initializing Google Calendar API:', error);
+            });
+        },
+
+        getAppointments() {
+            gapi.client.calendar.events.list({
+                calendarId: 'pavel@harelyshau.dev',
+                timeMin: (new Date()).toISOString(),
+                showDeleted: false,
+                singleEvents: true,
+                maxResults: 10,
+                orderBy: 'startTime'
+            }).then(function (response) {
+                const aAppointments = response.result.items;
+                console.log(aAppointments);
+            }, function (error) {
+                console.error('Error fetching events:', error);
+            });
         }
-    })
-})
+    });
+});
