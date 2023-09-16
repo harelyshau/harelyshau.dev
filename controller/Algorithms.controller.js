8ce4807886707e51e253f097e97b96730fa77962
Pavel Harelyshau <pavel@harelyshau.dev>
2023-09-16 11:41:02 +0400

    Add abort of request if another was called

--- controller/Algorithms.controller.js
+++ controller/Algorithms.controller.js
@@ -19,34 +19,54 @@ sap.ui.define(['./BaseController', '../model/models'],
         onRouteMatched(oEvent) {
             const sArticleId = oEvent.getParameter('arguments').articleId;
             this.setCurrentArticle(sArticleId);
+            this.getModel('view').setProperty('/ArticleID', sArticleId);
             if (!sArticleId) this.byId('sideNavigation').setSelectedItem(null);
         },
 
         async setCurrentArticle(sArticleId) {
+            this.getModel('view').setProperty('/busy', true);
+            try {
+                const oArticle = await this.getArticle(this.getArticlePath(sArticleId));
+                this.getModel().setProperty('/Article', oArticle);
+                this.getModel('view').setProperty('/busy', false);
+                document.title = oArticle.Title;
+            } catch (oError) {
+                if (oError.name === 'AbortError') return;
+                this.getModel().setProperty('/Article', { NotFound: true });
+                this.getModel('view').setProperty('/busy', false);
+            }
+        },
+
+        async getArticle(sArticlePath) {
+            if (this.oAbortContorller) this.oAbortContorller.abort();
+            this.oAbortContorller = new AbortController();
+            const oResponse = await fetch(sArticlePath, { signal: this.oAbortContorller.signal });
+            return await oResponse.json();
+        },
+
+        getArticlePath(sArticleId) {
             const sRootPath = 'resource/data/algorithms/';
             const sAdditionalPath = sArticleId ? `articles/${sArticleId}` : 'about';
-            const sPath = `${sRootPath}${sAdditionalPath}.json`;
-            try {
-                const oResponse = await fetch(sPath);
-                const oArticle = await oResponse.json();
-                this.getModel().setProperty('/Article', oArticle);
-                document.title = oArticle.Title;
-            } catch (e) {
-                this.getModel().setProperty('/Article', { ID: sArticleId, NotFound: true });
-            }
+            return `${sRootPath}${sAdditionalPath}.json`;
         },
 
         onSelectNavigateToArticle(oEvent) {
             const oItem = oEvent.getParameter('item');
             let articleId = this.getObjectByControl(oItem).ID;
             articleId ??= this.getObjectByControl(oItem.getItems()[0]).ID;
-            this.getRouter().navTo('Algorithm', {articleId});
+            this.getRouter().navTo('Algorithm', { articleId });
         },
 
         factoryBlocks(sId, oContext) {
             const sType = oContext.getProperty('Type');
-            const oBlock = this.byId(`block${sType}`).clone(sId);
-            return oBlock;
+            return this.byId(`block${sType}`).clone(sId);
+        },
+
+        refreshSideNavigationSelectedKey() {
+            setTimeout(() => {
+                const sKey = this.getModel('view').getProperty('/ArticleID');
+                this.byId('sideNavigation').setSelectedKey(sKey);
+            });
         }
 
 	});
