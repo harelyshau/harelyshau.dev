bed11db49b7edbadab8a3391369360e76ca68607
Pavel Harelyshau <pavel@harelyshau.dev>
2023-09-24 05:06:37 +0400

    Fix timer bug when change page

--- controller/HanoiTower.controller.js
+++ controller/HanoiTower.controller.js
@@ -16,6 +16,7 @@ sap.ui.define(
 				this.setupGame();
 				this.setDiscButtonMaxWidth();
 				ResizeHandler.register(this.getView(), this.setDiscButtonMaxWidth.bind(this));
+				this.getView().addEventDelegate({ onBeforeHide: this.stopTimer.bind(this) });
 			},
 
 			//////////////////////////////////
