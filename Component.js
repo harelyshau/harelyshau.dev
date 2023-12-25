bfc822d6f60ced2a88d9c10bc465543d8c4e5117
Pavel Harelyshau <pavel@harelyshau.dev>
2023-12-25 12:33:19 +0400

    Add fiori icons

--- Component.js
+++ Component.js
@@ -1,22 +1,35 @@
-sap.ui.define(
-	['sap/ui/core/UIComponent', './model/models', './util/themeHelper', './util/languageHelper'],
-	function (UIComponent, models, themeHelper, languageHelper) {
-		'use strict';
+sap.ui.define([
+	'sap/ui/core/UIComponent',
+	'./model/models',
+	'./util/themeHelper',
+	'./util/languageHelper',
+	'sap/ui/core/IconPool'
+], function (UIComponent, models, themeHelper, languageHelper, IconPool) {
+	'use strict';
 
-		return UIComponent.extend('pharelyshau.Component', {
-			metadata: {
-				interfaces: ['sap.ui.core.IAsyncContentCreation'],
-				manifest: 'json'
-			},
+	return UIComponent.extend('pharelyshau.Component', {
+		metadata: {
+			interfaces: ['sap.ui.core.IAsyncContentCreation'],
+			manifest: 'json'
+		},
 
-			init() {
-				// call the base component's init function and create the App view
-				UIComponent.prototype.init.apply(this, arguments);
-				this.getRouter().initialize();
-				this.setModel(models.createDeviceModel(), 'device');
-				languageHelper.initLanguage();
-				themeHelper.initTheme();
-			}
-		});
-	}
-);
+		init() {
+			// call the base component's init function and create the App view
+			UIComponent.prototype.init.apply(this, arguments);
+			this.getRouter().initialize();
+			this.setModel(models.createDeviceModel(), 'device');
+			languageHelper.initLanguage();
+			themeHelper.initTheme();
+			this.loadFioriIcons();
+		},
+
+		loadFioriIcons() {
+			const oFioriIconsFont = {
+				fontFamily: "SAP-icons-TNT",
+				fontURI: sap.ui.require.toUrl("sap/tnt/themes/base/fonts/")
+			};
+			IconPool.registerFont(oFioriIconsFont);
+		}
+
+	});
+});
--- controller/Minesweeper.controller.js
+++ controller/Minesweeper.controller.js
@@ -1,199 +1,188 @@
-sap.ui.define(
-	[
-		'./BaseController',
-		'sap/m/MessageToast',
-		'sap/m/InstanceManager',
-		'sap/ui/core/ResizeHandler',
-		'../model/models',
-        '../model/formatter',
-	],
-	(BaseController, MessageToast, InstanceManager, ResizeHandler, models, formatter) => {
-		'use strict';
+sap.ui.define([
+    './BaseController',
+    'sap/m/MessageToast',
+    'sap/m/InstanceManager',
+    'sap/ui/core/ResizeHandler',
+    '../model/models',
+    '../model/formatter',
+], (BaseController, MessageToast, InstanceManager, ResizeHandler, models, formatter) => {
+    'use strict';
 
-		return BaseController.extend('pharelyshau.controller.Minesweeper', {
-            formatter,
+    return BaseController.extend('pharelyshau.controller.Minesweeper', {
+        formatter,
 
-            onRightPressCell(oEvent) {
-                if (!this.isGameStarted()) return;
-                const sPath = this.getPathByEvent(oEvent) + '/IsFlagged';
-                const bFlagged = !this.getModel().getProperty(sPath);
-                this.getModel().setProperty(sPath, bFlagged);
-                this.updateFlagCount(bFlagged);
-                this.getModel().refresh(true);
-            },
+        onRightPressCell(oEvent) {
+            if (!this.isGameStarted()) return;
+            const sPath = this.getPathByEvent(oEvent) + '/IsFlagged';
+            const bFlagged = !this.getModel().getProperty(sPath);
+            this.getModel().setProperty(sPath, bFlagged);
+            this.updateFlagCount(bFlagged);
+            this.getModel().refresh(true);
+        },
 
-			onInit() {
-				this.setModel(models.createMinesweeperModel());
-				// this.setModel(models.createMinesweeperViewModel(), 'view');
-				this.setupGame();
-			},
+        onInit() {
+            this.setModel(models.createMinesweeperModel());
+            // this.setModel(models.createMinesweeperViewModel(), 'view');
+            this.setupGame();
+        },
 
-            setupGame() {
-                this.getModel().setProperty('/GameFinished', false);
-                this.getModel().setProperty('/Field', this.createField());
-                this.resetStartupParams();
-                this.stopTimer();
-            },
+        setupGame() {
+            this.getModel().setProperty('/GameFinished', false);
+            this.getModel().setProperty('/Field', this.createField());
+            this.resetStartupParams();
+            this.stopTimer();
+        },
 
-            resetStartupParams() {
-                const { Mines, Width, Height } = this.getCurrentLevel();
-                this.getModel().setProperty('/CellsLeft', Width * Height - Mines);
-                this.getModel().setProperty('/Flags', Mines);
-                this.getModel().setProperty('/Time', 0);
-            },
+        resetStartupParams() {
+            const { Mines, Width, Height } = this.getCurrentLevel();
+            this.getModel().setProperty('/CellsLeft', Width * Height - Mines);
+            this.getModel().setProperty('/Flags', Mines);
+            this.getModel().setProperty('/Time', 0);
+        },
 
-            createField() {
-                const { Width, Height } = this.getCurrentLevel();
-                return Array.from(
-                    { length: Height },
-                    (_, i) => Array.from(
-                        { length: Width },
-                        (_, j) => ({ ID: i * Width + j, Coordinates: [i, j] })
-                    )
-                );
-            },
+        createField() {
+            const { Width, Height } = this.getCurrentLevel();
+            return Array.from(
+                { length: Height },
+                (_, i) => Array.from(
+                    { length: Width },
+                    (_, j) => ({ ID: i * Width + j, Coordinates: [i, j] })
+                )
+            );
+        },
 
-            insertMines(iCurrentID) {
-                if (this.isAlreadyMined()) return;
-                const aField = this.getModel().getProperty('/Field');
-                const { Width, Height, Mines } = this.getCurrentLevel();
-                const aMineIndecies = [...Array(Width * Height).keys()]
-                    .filter(i => i !== iCurrentID)
-                    .sort(() => Math.random() - 0.5)
-                    .slice(0, Mines);
-                aField.forEach(aRow => aRow.forEach(
-                    oCell => oCell.IsMine = aMineIndecies.includes(oCell.ID)
-                ));
-                this.getModel().refresh();
-            },
+        insertMines(iCurrentID) {
+            if (this.isAlreadyMined()) return;
+            const aField = this.getModel().getProperty('/Field');
+            const { Width, Height, Mines } = this.getCurrentLevel();
+            const aMineIndecies = [...Array(Width * Height).keys()]
+                .filter(i => i !== iCurrentID)
+                .sort(() => Math.random() - 0.5)
+                .slice(0, Mines);
+            aField.forEach(aRow => aRow.forEach(
+                oCell => oCell.IsMine = aMineIndecies.includes(oCell.ID)
+            ));
+        },
 
-            isAlreadyMined() {
-                const aField = this.getField();
-                const oCell = aField[0][0];
-                return 'IsMine' in oCell;
-            },
+        isAlreadyMined() {
+            const aField = this.getField();
+            const oCell = aField[0][0];
+            return 'IsMine' in oCell;
+        },
 
-            getCurrentLevel() {
-                return this.getModel().getProperty('/Level');
-            },
+        getCurrentLevel() {
+            return this.getModel().getProperty('/Level');
+        },
 
-            onPressCell(oEvent) {
-                const oCell = this.getObjectByEvent(oEvent);
-                if (this.isGameFinished() || oCell.IsFlagged) return;
-                this.insertMines(this.getObjectByEvent(oEvent).ID);
-                this.openCells(this.getObjectByEvent(oEvent));
-                this.startTimer();
-                this.getModel().refresh(true);
-                const bGameLost = oCell.IsMine;
-                if (bGameLost || this.isGameWon()) this.finishGame(!bGameLost);
-            },
+        onPressCell(oEvent) {
+            const oCell = this.getObjectByEvent(oEvent);
+            if (this.isGameFinished() || oCell.IsFlagged) return;
+            this.insertMines(oCell.ID);
+            this.openCells(oCell);
+            this.startTimer();
+            this.getModel().refresh(true);
+            const bGameLost = oCell.IsMine;
+            if (bGameLost || this.isGameWon()) this.finishGame(!bGameLost);
+        },
 
-            finishGame(bWon) {
-                this.getModel().setProperty('/GameFinished', true);
-                MessageToast.show(bWon ? 'You won' : 'Game over');
-                this.showMines(bWon);
-                this.stopTimer();
-                if (bWon) this.getModel().setProperty('/Flags', 0);
-            },
+        finishGame(bWon) {
+            this.getModel().setProperty('/GameFinished', true);
+            MessageToast.show(bWon ? 'You won' : 'Game over');
+            this.showMines(bWon);
+            this.stopTimer();
+            if (bWon) this.getModel().setProperty('/Flags', 0);
+        },
 
-            isGameStarted() {
-                const iCellsLeft = this.getModel().getProperty('/CellsLeft');
-                const { Width, Height, Mines } = this.getCurrentLevel();
-				const bStarted = iCellsLeft !== Width * Height - Mines;
-				return bStarted && !this.isGameFinished();
-			},
+        isGameStarted() {
+            const iCellsLeft = this.getModel().getProperty('/CellsLeft');
+            const { Width, Height, Mines } = this.getCurrentLevel();
+            const bStarted = iCellsLeft !== Width * Height - Mines;
+            return bStarted && !this.isGameFinished();
+        },
 
-            isGameWon() {
-                return !this.getModel().getProperty('/CellsLeft');
-            },
+        isGameWon() {
+            return !this.getModel().getProperty('/CellsLeft');
+        },
 
-            isGameFinished() {
-                return this.getModel().getProperty('/GameFinished');
-            },
+        isGameFinished() {
+            return this.getModel().getProperty('/GameFinished');
+        },
 
-            showMines(bForceUpdate) {
-                const aMines = this.getField().flat().filter(oCell => oCell.IsMine);
-                aMines.forEach(oMine => this.openCell(oMine, true));
-                this.getModel().refresh(bForceUpdate);
-            },
+        showMines(bForceUpdate) {
+            const aCells = this.getField().flat();
+            const aMines = aCells.filter(oCell => oCell.IsMine && !oCell.IsFlagged);
+            aMines.forEach(oMine => this.openCell(oMine));
+            this.getModel().refresh(bForceUpdate);
+        },
 
-            openCells(oCell) {
-                oCell = this.openCell(oCell);
-                if (!oCell || oCell.MineCount) return;
-                const aNeighbours = this.getNeighbourCells(oCell);
-                aNeighbours.forEach(oCell => this.openCells(oCell));
-            },
+        openCells(oCell) {
+            oCell = this.openCell(oCell);
+            if (!oCell || oCell.MineCount || oCell.IsMine) return;
+            const aNeighbours = this.getNeighbourCells(oCell);
+            aNeighbours.forEach(oCell => this.openCells(oCell));
+        },
 
-            openCell(oCell, bKeepFlag) {
-                if (oCell.IsOpen) return;
-                const iCellsLeft = this.getModel().getProperty('/CellsLeft') - 1;
-                if (!oCell.IsMine) this.getModel().setProperty('/CellsLeft', iCellsLeft);
-                if (!bKeepFlag) {
-                    if (oCell.IsFlagged) this.updateFlagCount();
-                    oCell.IsFlagged = false;
-                }
-                oCell.IsOpen = true;
+        openCell(oCell, bKeepFlag) {
+            if (oCell.IsOpen) return;
+            const iCellsLeft = this.getModel().getProperty('/CellsLeft') - 1;
+            if (!oCell.IsMine) {
+                this.getModel().setProperty('/CellsLeft', iCellsLeft);
                 oCell.MineCount = this.getCellMineCount(oCell) || '';
-                return oCell;
-            },
+            };
+            if (oCell.IsFlagged) this.updateFlagCount();
+            oCell.IsFlagged = false;
+            oCell.IsOpen = true;
+            return oCell;
+        },
 
-            updateFlagCount(bDecrease) {
-                let iFlags = this.getModel().getProperty('/Flags');
-                iFlags -= bDecrease ? 1 : -1;
-                this.getModel().setProperty('/Flags', iFlags);
-            },
+        updateFlagCount(bDecrease) {
+            let iFlags = this.getModel().getProperty('/Flags');
+            iFlags -= bDecrease ? 1 : -1;
+            this.getModel().setProperty('/Flags', iFlags);
+        },
 
-            getCellMineCount(oCell) {
-                let iMineCount = 0;
-                const aDx = [-1, 0, 1];
-                aDx.forEach(iRowDx => {
-                    aDx.forEach(iColDx => {
-                        const [iCell, jCell] = oCell.Coordinates;
-                        const oNeighbour = this.getField()[iCell + iRowDx]?.[jCell + iColDx];
-                        if (oNeighbour?.IsMine) iMineCount++;
-                    });
+        getCellMineCount(oCell) {
+            const aNeighbours = this.getNeighbourCells(oCell);
+            return aNeighbours.filter(oCell => oCell.IsMine).length;
+        },
+
+        getNeighbourCells(oCell) {
+            const aField = this.getField();
+            const aDiff = [-1, 0, 1];
+            const aNeighbours = [];
+            aDiff.forEach(iRowDiff => {
+                aDiff.forEach(iColDiff => {
+                    const [x, y] = oCell.Coordinates;
+                    const oNeighbour = aField[x + iColDiff]?.[y + iRowDiff];
+                    if (oNeighbour) aNeighbours.push(oNeighbour);
                 });
-                return iMineCount;
-            },
+            });
+            return aNeighbours;
+        },
 
-            getNeighbourCells(oCell) {
-                const aField = this.getField();
-                const aDiff = [-1, 0, 1];
-                const aNeighbours = [];
-                aDiff.forEach(iRowDiff => {
-                    aDiff.forEach(iColDiff => {
-                        const [x, y] = oCell.Coordinates;
-                        const oNeighbour = aField[x + iColDiff]?.[y + iRowDiff];
-                        if (oNeighbour) aNeighbours.push(oNeighbour);
-                    });
-                });
-                return aNeighbours;
-            },
+        onPressRestartGame() {
+            this.setupGame();
+        },
 
-            onPressRestartGame() {
-                this.setupGame();
-            },
+        onChangeLevel(oEvent) {
+            const oLevel = this.getObjectByControl(oEvent.getParameter('selectedItem'));
+            this.setNewLevel(oLevel);
+        },
 
-            onChangeLevel(oEvent) {
-				const oLevel = this.getObjectByControl(oEvent.getParameter('selectedItem'));
-				this.setNewLevel(oLevel);
-			},
+        setNewLevel(oLevel) {
+            this.getModel().setProperty('/Level', oLevel);
+            this.setupGame();
+        },
 
-            setNewLevel(oLevel) {
-                this.getModel().setProperty('/Level', oLevel);
-                this.setupGame();
-            },
+        getField() {
+            return this.getModel().getProperty('/Field')
+        },
 
-            getField() {
-                return this.getModel().getProperty('/Field')
-            },
-
-            onPressCloseAndRestart(oEvent) {
-                this.oGameOverDialog.close();
-                this.setupGame();
-            }
+        onPressCloseAndRestart(oEvent) {
+            this.oGameOverDialog.close();
+            this.setupGame();
+        }
 
 
-		});
-	}
-);
+    });
+});
