e41ad215c39440800c5cbb8ba6ee3316a4ddd2be
Pavel Harelyshau <pavel@harelyshau.dev>
2024-02-10 23:39:42 +0400

    Remove extra tab

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -5,60 +5,59 @@ sap.ui.define([
 	'../util/languageHelper',
 	'sap/ui/core/ResizeHandler',
 ], (BaseController, models, themeHelper, languageHelper, ResizeHandler) => {
-		'use strict';
+	'use strict';
 
-		return BaseController.extend('pharelyshau.controller.App', {
+	return BaseController.extend('pharelyshau.controller.App', {
 
-			onInit() {
-				this.addContentDensityClass(this.getView());
-				this.setModel(models.createAppModel(), 'app');
-				this.getRouter().attachRouteMatched(this.onRouteMatched.bind(this));
-				this.getRouter().attachTitleChanged(this.onTitleChanged.bind(this));
-				themeHelper.attachChange(() => {
-					this.setProperty('/theme', themeHelper.getTheme(), 'app');
-				});
-				this.attachToResize();
-			},
+		onInit() {
+			this.addContentDensityClass(this.getView());
+			this.setModel(models.createAppModel(), 'app');
+			this.getRouter().attachRouteMatched(this.onRouteMatched.bind(this));
+			this.getRouter().attachTitleChanged(this.onTitleChanged.bind(this));
+			themeHelper.attachChange(() => {
+				this.setProperty('/theme', themeHelper.getTheme(), 'app');
+			});
+			this.attachToResize();
+		},
 
-			attachToResize() {
-				ResizeHandler.register(
-					this.getView(),
-					() => this.setModel(models.createDeviceModel(), 'device')
-				);
-			},
+		attachToResize() {
+			ResizeHandler.register(
+				this.getView(),
+				() => this.setModel(models.createDeviceModel(), 'device')
+			);
+		},
 
-			onRouteMatched(oEvent) {
-				const sPage = oEvent.getParameter('config').target;
-				this.setProperty('/page', sPage, 'app');
-				this.byId('page').setSideExpanded(false);
-			},
+		onRouteMatched(oEvent) {
+			const sPage = oEvent.getParameter('config').target;
+			this.setProperty('/page', sPage, 'app');
+			this.byId('page').setSideExpanded(false);
+		},
 
-			onTitleChanged(oEvent) {
-				document.title = oEvent.getParameter('title');
-			},
+		onTitleChanged(oEvent) {
+			document.title = oEvent.getParameter('title');
+		},
 
-			onSelectPage(oEvent) {
-				this.getRouter().navTo(oEvent.getParameter('item').getKey());
-			},
+		onSelectPage(oEvent) {
+			this.getRouter().navTo(oEvent.getParameter('item').getKey());
+		},
 
-			onPressToggleTheme() {
-				const sCurrentTheme = this.getProperty('/theme', 'app');
-				const oOppositeThemes = { light: 'dark', dark: 'light' };
-				const sTheme = oOppositeThemes[sCurrentTheme];
-				themeHelper.setTheme(sTheme);
-				this.setProperty('/theme', sTheme, 'app');
-			},
+		onPressToggleTheme() {
+			const sCurrentTheme = this.getProperty('/theme', 'app');
+			const oOppositeThemes = { light: 'dark', dark: 'light' };
+			const sTheme = oOppositeThemes[sCurrentTheme];
+			themeHelper.setTheme(sTheme);
+			this.setProperty('/theme', sTheme, 'app');
+		},
 
-			onSelectLanguage(oEvent) {
-				languageHelper.setLanguage(oEvent.getParameter('item').getKey());
-				// window.location.reload(); // need to refresh to change controls language
-			},
+		onSelectLanguage(oEvent) {
+			languageHelper.setLanguage(oEvent.getParameter('item').getKey());
+			// window.location.reload(); // need to refresh to change controls language
+		},
 
-			onPressToggleSideNavigation() {
-				const oPage = this.byId('page');
-				this.toggleSideNavigation(oPage, this.byId('sideNavigation'));
-			}
+		onPressToggleSideNavigation() {
+			const oPage = this.byId('page');
+			this.toggleSideNavigation(oPage, this.byId('sideNavigation'));
+		}
 
-		});
-	}
-);
+	});
+});
