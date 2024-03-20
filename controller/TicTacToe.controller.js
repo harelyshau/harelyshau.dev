8e5e238015d8c864e4a03e7c0244a1f9dd138f89
Pavel Harelyshau <pavel@harelyshau.dev>
2024-03-20 15:39:23 +0400

    Fix for tic-tac-toe

--- controller/TicTacToe.controller.js
+++ controller/TicTacToe.controller.js
@@ -16,11 +16,15 @@ sap.ui.define([
         onTicTacToeMatched(oEvent) {
             const { level } = oEvent.getParameter('arguments');
             const aLevels = this.getProperty('/levels');
-            aLevels.some(({ key }) => key === level)
+            const bLevelExists = aLevels.some(({ key }) => key === level);
+            const bAntoherLevel = level !== this.getProperty('/level');
+            bLevelExists
                 ? this.setProperty('/level', level)
                 : this.navigateTo('TicTacToe');
-            this.resetScore();
-            this.setupGame();
+            if (bLevelExists && bAntoherLevel) {
+                this.resetScore();
+                this.setupGame();
+            }
         },
 
         resetScore() {
